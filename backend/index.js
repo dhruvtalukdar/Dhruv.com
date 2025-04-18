d84d8cd321e3d0c4c647db57146d758afa436273
Dhrubajyoti Talukdar <dhrubajyoti.talukdar@siemens.com>
2025-04-18 16:27:15 +0530

    axios and cors added

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import contactRoute from "./routes/contact.route.js";
 import connectDb from "./config/mongoose-connection.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -11,6 +12,13 @@ const app = express();
 
 app.use(express.json());
 
+// cors
+app.use(cors({
+    origin: 'http://localhost:8081',
+    credentials: true,
+}));
+
+// routes
 app.use('/api', contactRoute);
 
 app.use('/', (req, res) => {
